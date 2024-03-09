1ac70f0037db04a5e7ba8fbc15ee7697cf3cf57a
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-03-09 17:41:26 +0200

    project setup requirements

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="d-flex bg-warning">
+      <h1>Hello World</h1>
+      <p>Hi there</p>
+    </div>
+  );
+}
+
+export default Home;
